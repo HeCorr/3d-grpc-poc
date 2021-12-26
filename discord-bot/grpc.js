8f4aa4db503bded1ae42e310e293cc234cdd2a62
HeCorr <hccaseclone@gmail.com>
2021-12-26 03:01:36 -0300

    update requestRender() to take in objects

--- discord-bot/grpc.js
+++ discord-bot/grpc.js
@@ -15,24 +15,10 @@ var renderer = grpc.loadPackageDefinition(packageDefinition).render_server;
 
 var client = new renderer.Renderer('localhost:1313', grpc.credentials.createInsecure());
 
-function requestRender() {
+function requestRender(objects) {
     return new Promise((resolve, reject) => {
         client.RequestRender({
-            objects: [
-                {
-                    shape: "Cube",
-                    position: {
-                        x: 0,
-                        y: 0,
-                        z: 0,
-                    },
-                    scale: {
-                        x: 1,
-                        y: 1,
-                        z: 1,
-                    }
-                }
-            ]
+            objects: objects
         }, (err, resp) => {
             if (err) reject(err);
             resolve(resp.imageBytes)
